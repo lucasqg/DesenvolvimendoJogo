22284750ecc97f86f4a9e0be7746b83585f6da29
Lucas Garcia <lqgarcia@inf.ufpel.edu.br>
2018-06-25 16:09:30 -0300

    Atualização final

---
+++ JogoPrincipal/RPG/Assets/Menu/Scripts/Audio/AudioG.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioG : MonoBehaviour {
+
+    public AudioSource Sons;
+    public static AudioG inst = null;
+
+    private void Awake()
+    {
+        if(inst == null)
+        {
+            inst = this;
+        }else if (inst != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayAudio(AudioClip clipAudio)
+    {
+        Sons.clip = clipAudio;
+        Sons.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
