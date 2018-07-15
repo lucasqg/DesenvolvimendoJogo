331861a7b870498777a3e18f28ca41eabcfec197
skileh <heliksmm@hotmail.com>
2018-07-15 19:29:30 -0300

    Text mech?

---
+++ JogoPrincipal/RPG/Assets/GamePlay/Scripts/UI/Minimap/MovimentoCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoCamera : MonoBehaviour {
+
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 NovaPosicao = player.position;
+        NovaPosicao.y = player.position.y;
+        transform.position = NovaPosicao;
+
+    }
+}
